dc2ecbae85e166b24a95906df20483409732a450
Florian Mörzinger <flo.moerzinger@gmx.at>
2020-02-18 13:30:03 +0100

    json

---
+++ advanced_json.js
@@ -0,0 +1,64 @@
+
+var setMenu = JSON.parse(`{
+	"menu":[
+	{
+		"id":"1",
+		"name":"Schweinsbraten",
+		"image":"./img/schweinsbraten.jpg",
+		"info":"viel Schwein wenig Salad",
+		"price":"8"
+		
+	},	{
+		"id":"2",
+		"name":"Schweinsbraten",
+		"image":"./img/schweinsbraten.jpg",
+		"info":"viel Schwein wenig Salad",
+		"price":"8"
+		
+	},	{
+		"id":"3",
+		"name":"Schweinsbraten",
+		"image":"./img/schweinsbraten.jpg",
+		"info":"viel Schwein wenig Salad",
+		"price":"8"
+		
+	},	{
+		"id":"4",
+		"name":"Schweinsbraten",
+		"image":"./img/schweinsbraten.jpg",
+		"info":"viel Schwein wenig Salad",
+		"price":"8"
+		
+	},	{
+		"id":"5",
+		"name":"Schweinsbraten",
+		"image":"./img/schweinsbraten.jpg",
+		"info":"viel Schwein wenig Salad",
+		"price":"8"
+		
+	},	{
+		"id":"6",
+		"name":"Schweinsbraten",
+		"image":"./img/schweinsbraten.jpg",
+		"info":"viel Schwein wenig Salad",
+		"price":"8"
+		
+	},	{
+		"id":"7",
+		"name":"Schweinsbraten",
+		"image":"./img/schweinsbraten.jpg",
+		"info":"viel Schwein wenig Salad",
+		"price":"8"
+		
+	},	{
+		"id":"8",
+		"name":"Schweinsbraten",
+		"image":"./img/schweinsbraten.jpg",
+		"info":"viel Schwein wenig Salad",
+		"price":"8"
+		
+	},
+
+
+]
+}`)
