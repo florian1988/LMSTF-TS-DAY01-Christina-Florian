ae90b73cf5d2c9cf5283ed3d01538c8d9e1f2d69
Florian Mörzinger <flo.moerzinger@gmx.at>
2020-02-18 11:58:41 +0100

    fist

---
+++ basic.index.js
@@ -0,0 +1,17 @@
+console.log("hallo");
+var list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// document.write(list);
+var number = 1;
+for (var i = 0; i < list.length; i++) {
+    document.write(list[i] + " x " + number + " = " + (list[i] * 1) + "<br>");
+}
+document.write("<br>");
+var number = 2;
+for (var i = 0; i < list.length; i++) {
+    document.write(list[i] + " x " + number + " = " + (list[i] * 2) + "<br>");
+}
+document.write("<br>");
+var number = 10;
+for (var i = 0; i < list.length; i++) {
+    document.write(list[i] + " x " + number + " = " + (list[i] * 2) + "<br>");
+}
---
+++ basic.index.ts
@@ -0,0 +1,24 @@
+console.log("hallo");
+
+
+
+let list: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// document.write(list);
+
+
+var number = 1;
+for (let i = 0; i < list.length; i++){
+	document.write(list[i] +" x "+ number  +" = "+( list[i]*1) +"<br>");
+}
+document.write("<br>");
+var number = 2;
+for (let i = 0; i < list.length; i++){
+	document.write(list[i] +" x "+ number  +" = "+( list[i]*2) +"<br>");
+}
+document.write("<br>");
+
+var number = 10;
+for (let i = 0; i < list.length; i++){
+	document.write(list[i] +" x "+ number  +" = "+( list[i]*2) +"<br>");
+}
